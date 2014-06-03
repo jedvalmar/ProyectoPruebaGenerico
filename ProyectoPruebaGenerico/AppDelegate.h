f3e3ccc99581eb57fe40d188cbb6a37e18982f49
Eduardo Valenzuela <lalo@ievolutioned.com>
2014-06-03 18:47:41 -0500

    Initial Commit

---
+++ ProyectoPruebaGenerico/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProyectoPruebaGenerico
+//
+//  Created by Eduardo Valenzuela on 6/3/14.
+//  Copyright (c) 2014 Eduardo Valenzuela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
