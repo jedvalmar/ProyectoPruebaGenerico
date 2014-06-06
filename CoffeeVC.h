0697bfe0cd663e2976b00f7a15a7cb6ca03d3df2
fawkes32 <dmarquez@Daniels-MacBook-Pro.local>
2014-06-05 22:33:18 -0500

    navController pushed View

---
+++ CoffeeVC.h
@@ -0,0 +1,15 @@
+//
+//  CoffeeVC.h
+//  ProyectoPruebaGenerico
+//
+//  Created by Daniel Márquez on 6/5/14.
+//  Copyright (c) 2014 Eduardo Valenzuela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoffeeVC : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lblTitle;
+@property (weak, nonatomic) IBOutlet UIImageView *imgCoffee;
+
+@end
