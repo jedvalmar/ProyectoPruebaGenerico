19f3616c36b31b8a270f8411758bc007e8ea1e88
fawkes32 <dmarquez@Daniels-MacBook-Pro.local>
2014-06-09 19:11:51 -0500

    table view and custom cells

---
+++ MyCustomCell.m
@@ -0,0 +1,25 @@
+//
+//  MyCustomCell.m
+//  ProyectoPruebaGenerico
+//
+//  Created by Daniel Márquez on 6/9/14.
+//  Copyright (c) 2014 Eduardo Valenzuela. All rights reserved.
+//
+
+#import "MyCustomCell.h"
+
+@implementation MyCustomCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
